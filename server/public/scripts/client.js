effe2615690c342b030433d495245f2576974450
Jaffa Aharonov <jaharonov@gmail.com>
2017-12-08 15:11:36 -0600

    moved login and logout

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -14,7 +14,7 @@ myApp.config(function($routeProvider, $locationProvider) {
       controller: 'LoginController as lc'
     })
     .when('/user', {
-      templateUrl: '/views/templates/user.html',
+      templateUrl: '/views/templates/dashboard.html',
       controller: 'UserController as uc',
       resolve: {
         getuser : function(UserService){
