3ca1091c1bfb806ee524e027e1705a19f4e44eea
Zack Stout <zackstout@gmail.com>
2017-12-11 13:48:42 -0600

    trash stuff

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -35,3 +35,38 @@ myApp.config(function($routeProvider, $locationProvider) {
       redirectTo: 'home'
     });
 });
+
+
+myApp.directive('fileModel', ['$parse', function ($parse) {
+   return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+         var model = $parse(attrs.fileModel);
+         var modelSetter = model.assign;
+
+         element.bind('change', function(){
+            scope.$apply(function(){
+               modelSetter(scope, element[0].files[0]);
+            });
+         });
+      }
+   };
+}]);
+
+myApp.service('fileUpload', ['$https:', function ($https) {
+   this.uploadFileToUrl = function(file, uploadUrl){
+      var fd = new FormData();
+      fd.append('file', file);
+
+      $https.post(uploadUrl, fd, {
+         transformRequest: angular.identity,
+         headers: {'Content-Type': undefined}
+      })
+
+      .success(function(){
+      })
+
+      .error(function(){
+      });
+   }
+}]);
--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -1,6 +1,22 @@
-myApp.controller('UserController', function(UserService) {
+myApp.controller('UserController', function(UserService, fileUpload) {
   console.log('UserController created');
   var vm = this;
   vm.userService = UserService;
   vm.userObject = UserService.userObject;
+
+
+
+
+
+      vm.uploadFile = function(){
+         var file = vm.myFile;
+
+         console.log('file is ' );
+         console.dir(file);
+
+         var uploadUrl = "/fileUpload";
+         fileUpload.uploadFileToUrl(file, uploadUrl);
+      };
+
+
 });
