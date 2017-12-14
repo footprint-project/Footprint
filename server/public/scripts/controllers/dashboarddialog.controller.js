f236fc55b4915affd5d5e91091d4e023a6090022
Blaze Fugina <blaze.fugina@gmail.com>
2017-12-14 12:19:19 -0600

    updates to dialog box.

--- server/public/scripts/controllers/dashboarddialog.controller.js
+++ server/public/scripts/controllers/dashboarddialog.controller.js
@@ -1,8 +1,18 @@
-myApp.controller('DashboardDialogController', function (UserService, $mdDialog) {
+myApp.controller('DashboardDialogController', function (UserService, csvService, $mdDialog, $interval) {
     console.log('DashboardDialogController created');
     var vm = this;
     vm.userService = UserService;
     vm.userObject = UserService.userObject;
     vm.countries = UserService.countries.data;
+    vm.months = UserService.months;
+    vm.countries = UserService.countries.data;
+    vm.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+     'September', 'October', 'November', 'December'];
+    vm.years = [2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011];
+    vm.items = ['Health', 'Food/Nutrition', 'Education', 'Non-Food Items (NFI)', 'Shelter', 'Conflict', 'Migration/Camp Management', 'Faith-based', 'Research', 'Governance', 'Business/Entrepeneur', 'Donor'];
 
-});
+
+
+
+
+}); //End Dialog controller
