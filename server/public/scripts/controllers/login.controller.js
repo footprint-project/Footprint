bea34eb6fd791f139fcab64d610ec3b225f99496
Zack Stout <zackstout@gmail.com>
2017-12-20 16:12:29 -0600

    whatup

--- server/public/scripts/controllers/login.controller.js
+++ server/public/scripts/controllers/login.controller.js
@@ -30,39 +30,7 @@
   //      vm.selected.splice(vm.selected.indexOf(item), 1);
   //    }
   //  };
-  vm.computedFps = [];
-
-  function checkAdminRoute() {
-    $http.get('/admin/users').then(function(response) {
-      for (var i=0; i < response.data.totals.length; i++) {
-        var fp = response.data.totals[i];
-        var fpComp = UserService.computeFootprint(fp);
-        var total = fpComp.air + fpComp.truck + fpComp.sea + fpComp.freight_train + fpComp.propane + fpComp.fuel + fpComp.grid + fpComp.hotel + fpComp.train + fpComp.car + fpComp.plane;
-        var organization = fpComp.organization;
-        var fpObj = {
-          total: total,
-          organization: organization
-        };
-        vm.computedFps.push(fpObj);
-      }
-      for (var j=0; j<vm.computedFps.length; j++) {
-        for (var k=0; k<response.data.otherStuff.length; k++) {
-          var other = response.data.otherStuff[k];
-          var fpc = vm.computedFps[j];
-          if (other.organization == fpc.organization) {
-            fpc.name = other.name;
-            fpc.position = other.position;
-            fpc.username = other.username;
-          }
-        }
-      }
-      console.log(vm.computedFps);
-    }).catch(function(err) {
-      console.log(err);
-    });
-  }
-
-  checkAdminRoute();
+  
 
     //this is called in the lineChart function, not sure why it's called here...
    //donutService.getFpDividedByPeriod(1);
